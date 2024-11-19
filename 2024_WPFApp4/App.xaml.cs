f81265f7cc98133d5f3d77859a75f02a3a66b988
0315huifeng <4b2g0045@stust.edu.tw>
2024-11-19 15:17:48 +0800

    在 `2024_WpfApp4.csproj` 中，將多個影像檔案（`ellipse.png`、`eraser.png`、`line.png`、`polyline.png`、`rectangle.png`、`trashcan.png` ）的`None`標籤改為`Resource`標籤。 在`MainWindow.xaml`中，更新視窗標題為“WPF小畫家ver2”，調整視窗和視力的高度和寬度。 擴展工具列寬度，新增填滿色彩選擇器、筆刷粗細滑桿及其顯示標籤。 新增一個包含多個形狀選擇按鈕、橡皮擦按鈕和清除按鈕的新工具欄，按鈕使用對應圖像作為圖示。 在 `MainWindow.xaml.cs` 中，新增 `System.Windows.Controls` 的引用，新增 `fillColor`、`fillBrush`、`shape` 和 `linesThickness` 變數，並初始化 `fillColorPicker` 的選擇顏色。 加入 `fillColorPicker_SelectedColorChanged`、`ShapeButton_Click`、`EraseButton_Click`、`ClearButton_Click` 和 `styleThicknessSlider_ValueChanged` 方法來處理對應事件。

--- 2024_WPFApp4/App.xaml.cs
+++ 2024_WPFApp4/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace _2024_WPFApp4
+namespace _2024_WpfApp4
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- 2024_WPFApp4/MainWindow.xaml.cs
+++ 2024_WPFApp4/MainWindow.xaml.cs
@@ -1,99 +1,91 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
-namespace _2024_WPFApp4
+namespace _2024_WpfApp4
 {
     /// <summary>
-    /// 主視窗的互動邏輯
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        // 畫筆顏色和畫筆畫刷的初始值
-        Color strokecolor = Colors.Black;
-        Color fillcolor = Colors.Aqua;
-        Brush strokebrush = Brushes.Black;
-        Brush fillbrush = Brushes.Aqua;
-        string shape;
-        int strokeThickness = 2;
-        
-        // 起始點和終點的座標
+        Color strokeColor = Colors.Black;
+        Color fillColor = Colors.Aqua;
+        Brush strokeBrush = Brushes.Black;
+        Brush fillBrush = Brushes.Aqua;
+        string shapeType = "line";
+        int strokeThickness = 1;
         Point start, dest;
-
-        // 主視窗建構函式
         public MainWindow()
         {
             InitializeComponent();
-            // 初始化顏色選擇器為黑色
-            strokeColorPicker.SelectedColor = strokecolor;
-            fillColorPicker.SelectedColor = fillcolor;
+            strokeColorPicker.SelectedColor = strokeColor;
+            fillColorPicker.SelectedColor = fillColor;
         }
 
-        /// <summary>
-        /// 滑鼠進入畫布時更改滑鼠游標為畫筆圖示
-        /// </summary>
         private void myCanvas_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             myCanvas.Cursor = Cursors.Pen;
         }
 
-        /// <summary>
-        /// 當滑鼠在畫布上移動時，不斷更新終點座標並顯示起點與終點的座標
-        /// </summary>
         private void myCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-            // 更新終點座標
             dest = e.GetPosition(myCanvas);
-            // 更新狀態欄顯示的座標資訊
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point origin;
+                origin.X = Math.Min(start.X, dest.X);
+                origin.Y = Math.Min(start.Y, dest.Y);
+                double width = Math.Abs(start.X - dest.X);
+                double height = Math.Abs(start.Y - dest.Y);
+
+                switch (shapeType)
+                {
+                    case "line":
+                        var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                        line.X2 = dest.X;
+                        line.Y2 = dest.Y;
+                        break;
+                    case "rectangle":
+                        var rectangle = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                        rectangle.Width = width;
+                        rectangle.Height = height;
+                        rectangle.SetValue(Canvas.LeftProperty, origin.X);
+                        rectangle.SetValue(Canvas.TopProperty, origin.Y);
+                        break;
+                    case "ellipse":
+                        var ellipse = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                        ellipse.Width = width;
+                        ellipse.Height = height;
+                        ellipse.SetValue(Canvas.LeftProperty, origin.X);
+                        ellipse.SetValue(Canvas.TopProperty, origin.Y);
+                        break;
+                    case "polyline":
+                        break;
+                }
+            }
+
+
+
             statusPoint.Content = $"({Convert.ToInt32(start.X)}, {Convert.ToInt32(start.Y)}) - ({Convert.ToInt32(dest.X)}, {Convert.ToInt32(dest.Y)})";
         }
 
-        /// <summary>
-        /// 當滑鼠左鍵在畫布上放開時，繪製從起點到終點的直線
-        /// </summary>
-        private void myCanvas_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            // 使用選擇的顏色創建畫筆畫刷
-            var brush = new SolidColorBrush(strokecolor);
-
-            // 創建並設定直線的屬性
-            Line line = new Line()
-            {
-                X1 = start.X,            // 起點 X 座標
-                Y1 = start.Y,            // 起點 Y 座標
-                X2 = dest.X,             // 終點 X 座標
-                Y2 = dest.Y,             // 終點 Y 座標
-                Stroke = brush,          // 使用選擇的顏色填充直線
-                StrokeThickness = 2      // 設定線條寬度為 2
-            };
-
-            // 將直線添加到畫布上
-            myCanvas.Children.Add(line);
-        }
-
-        /// <summary>
-        /// 當顏色選擇器的顏色改變時，更新直線的畫筆顏色
-        /// </summary>
         private void strokeColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
-            // 更新畫筆顏色為選擇器選中的顏色
-            strokecolor = strokeColorPicker.SelectedColor.Value;
+            strokeColor = strokeColorPicker.SelectedColor.Value;
         }
 
         private void fillColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
-            fillcolor = fillColorPicker.SelectedColor.Value;
+            fillColor = fillColorPicker.SelectedColor.Value;
         }
 
         private void ShapeButton_Click(object sender, RoutedEventArgs e)
         {
-
-            var button = sender as RadioButton;
-            shape = button.Tag.ToString();
-            MessageBox.Show(shape);
+            var targetRadioButton = sender as RadioButton;
+            shapeType = targetRadioButton.Tag.ToString();
         }
 
         private void EraseButton_Click(object sender, RoutedEventArgs e)
@@ -108,18 +100,80 @@ namespace _2024_WPFApp4
 
         private void strokeThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            strokeThickness = (int) strokeThicknessSlider.Value; 
+            strokeThickness = (int)strokeThicknessSlider.Value;
         }
 
-        /// <summary>
-        /// 當滑鼠左鍵按下時，記錄起點座標並更改游標為十字圖示
-        /// </summary>
+        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Brush strokeBrush = new SolidColorBrush(strokeColor);
+            Brush fillBrush = new SolidColorBrush(fillColor);
+
+            switch (shapeType)
+            {
+                case "line":
+                    var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                    line.Stroke = strokeBrush;
+                    line.StrokeThickness = strokeThickness;
+                    break;
+                case "rectangle":
+                    var rectangle = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                    rectangle.Stroke = strokeBrush;
+                    rectangle.Fill = fillBrush;
+                    rectangle.StrokeThickness = strokeThickness;
+                    break;
+                case "ellipse":
+                    var ellipse = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                    ellipse.Stroke = strokeBrush;
+                    ellipse.Fill = fillBrush;
+                    ellipse.StrokeThickness = strokeThickness;
+                    break;
+                case "polyline":
+                    break;
+            }
+        }
+
         private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // 獲取起點座標
             start = e.GetPosition(myCanvas);
-            // 更改游標為十字圖示
             myCanvas.Cursor = Cursors.Cross;
+
+            switch (shapeType)
+            {
+                case "line":
+                    Line line = new Line
+                    {
+                        X1 = start.X,
+                        Y1 = start.Y,
+                        X2 = dest.X,
+                        Y2 = dest.Y,
+                        Stroke = Brushes.Gray,
+                        StrokeThickness = 1
+                    };
+                    myCanvas.Children.Add(line);
+                    break;
+                case "rectangle":
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Stroke = Brushes.Gray,
+                        Fill = Brushes.LightGray
+                    };
+                    myCanvas.Children.Add(rectangle);
+                    rectangle.SetValue(Canvas.LeftProperty, start.X);
+                    rectangle.SetValue(Canvas.TopProperty, start.Y);
+                    break;
+                case "ellipse":
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Stroke = Brushes.Gray,
+                        Fill = Brushes.LightGray
+                    };
+                    myCanvas.Children.Add(ellipse);
+                    ellipse.SetValue(Canvas.LeftProperty, start.X);
+                    ellipse.SetValue(Canvas.TopProperty, start.Y);
+                    break;
+                case "polyline":
+                    break;
+            }
         }
     }
 }
